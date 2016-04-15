7b06d2e7a3427fb64b042e106b63dac41044d0f7
Mulkey, Rodrigo S <rmulkey@gmail.com>
2016-04-15 11:52:42 -0400

    initial commit;

---
+++ BeaconYourPet/BeaconYourPet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeaconYourPet
+//
+//  Created by Mulkey, Rodrigo S. on 4/15/16.
+//  Copyright © 2016 Mulkey, Rodrigo S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BeaconYourPet/BeaconYourPetTests/BeaconYourPetTests.m
@@ -0,0 +1,39 @@
+//
+//  BeaconYourPetTests.m
+//  BeaconYourPetTests
+//
+//  Created by Mulkey, Rodrigo S. on 4/15/16.
+//  Copyright © 2016 Mulkey, Rodrigo S. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BeaconYourPetTests : XCTestCase
+
+@end
+
+@implementation BeaconYourPetTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
